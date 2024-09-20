cbfab43a6868055e4b1104eaa3c360e0307de6f5
akhi2time <m.mohamedsandiego@gmail.com>
2024-09-19 21:48:42 -0500

    Simplify JS Code

--- script.js
+++ script.js
@@ -1,42 +1,12 @@
 // Convert minutes to units
 function convertMinutesToUnits(minutes) {
-    if (minutes < 8) {
-        return 0; // Minutes less than 8 are 0 units
-    } else if (minutes <= 22) {
-        return 1;
-    } else if (minutes <= 37) {
-        return 2;
-    } else if (minutes <= 52) {
-        return 3;
-    } else if (minutes <= 67) {
-        return 4;
-    } else if (minutes <= 82) {
-        return 5;
-    } else if (minutes <= 97) {
-        return 6;
-    } else if (minutes <= 112) {
-        return 7;
-    } else if (minutes <= 127) {
-        return 8;
-    } else if (minutes <= 142) {
-        return 9;
-    } else if (minutes <= 157) {
-        return 10;
-    } else if (minutes <= 172) {
-        return 11;
-    } else if (minutes <= 187) {
-        return 12;
-    } else if (minutes <= 202) {
-        return 13;
-    } else if (minutes <= 217) {
-        return 14;
-    } else if (minutes <= 232) {
-        return 15;
-    } else if (minutes <= 247) {
-        return 16;
-    } else {
-        return "Invalid input: Exceeds the defined range.";
+    const unitRanges = [8, 22, 37, 52, 67, 82, 97, 112, 127, 142, 157, 172, 187, 202, 217, 232, 247];
+    for (let i = 0; i < unitRanges.length; i++) {
+        if (minutes < unitRanges[i]) {
+            return i;
+        }
     }
+    return "Invalid input: Exceeds the defined range.";
 }
 
 // Convert time (HH:MM) to total minutes
